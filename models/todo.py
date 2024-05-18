89495e0ac10575c2d593c01aa66463f30d08451f
kurunin52 <kurunin52@gmail.com>
2024-05-18 23:43:06 +0900

    feat: add basic files

---
+++ models/todo.py
@@ -0,0 +1,11 @@
+import json
+
+class Todo:
+    def __init__(self, id_=None, content=None, done=None):
+        self.id = id_
+        self.content = content
+        self.done = done
+
+    def __str__(self):
+        return json.dumps(self.__dict__, ensure_ascii=False)
+    
