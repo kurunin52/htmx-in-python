0b2faf9d105e04647466b395b132519040d0a7fa
kurunin52 <33592045+kurunin52@users.noreply.github.com>
2024-05-19 09:37:30 +0000

    feat: add db connection settings

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask
+import os
+from flask import Flask, render_template
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
 
+CONNECTION_STRING = os.environ.get("AZURE_COSMOS_CONNECTIONSTRING")
+client: MongoClient = MongoClient(CONNECTION_STRING)
+
+db = client.get_database("db_todo")
+todos = db.todos
+
+
 @app.route("/")
 def hello_world():
-    return "<p>Hello, world!</p>"
+    list = todos.find()
+    return render_template("index.html", todos=list)
 
 
 if __name__ == "__main__":
