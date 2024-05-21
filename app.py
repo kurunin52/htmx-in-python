64390a202695fb5dfb1a996ff73906e0b8e0760a
kurunin52 <33592045+kurunin52@users.noreply.github.com>
2024-05-21 14:48:50 +0000

    feat: add base books page

--- app.py
+++ app.py
@@ -6,22 +6,32 @@ app.secret_key = b"hypermedia journey"
 
 
 @app.route("/")
-def hello_world():
+def index_page():
+    return render_template("index.html")
+
+
+@app.route("/books")
+def books_page():
+    return render_template("books/index.html", todos=list)
+
+
+@app.route("/todo")
+def todo_page():
     list = Todo.all()
-    return render_template("index.html", todos=list)
+    return render_template("todo/index.html", todos=list)
 
 
-@app.route("/edit", methods=["GET"])
-def get_edit_row():
-    return render_template("editRow.html", todo={})
+@app.route("/todo/edit", methods=["GET"])
+def get_todo_edit_row():
+    return render_template("todo/editForm.html", todo={})
 
 
-@app.route("/edit", methods=["POST"])
-def save_new():
+@app.route("/todo/edit", methods=["POST"])
+def save_new_todo():
     todo = Todo(content=request.form["content"])
     todo = todo.save_new()
     flash("new task is created!")
-    return render_template("rows.html", todos=[todo])
+    return render_template("todo/rows.html", todos=[todo])
 
 
 if __name__ == "__main__":
